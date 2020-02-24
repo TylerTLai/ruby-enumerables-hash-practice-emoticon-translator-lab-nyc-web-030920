30f39b5f7a82b8642e316e75b359d1da10fe8761
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-24 20:12:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,42 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  
+  emoticons_yml = YAML.load_file(path)
+  
+  new_emoticons = {}
+  
+  new_emoticons["get_meaning"] = {}
+  new_emoticons["get_emoticon"] = {}
+  
+  emoticons_yml.each do |meaning, emoticon|
+    
+    new_emoticons["get_meaning"][emoticon[1]] = meaning
+    new_emoticons["get_emoticon"][emoticon[0]] = emoticon[1]
+
+  end 
+  
+  new_emoticons
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+# binding.pry
+
+def get_japanese_emoticon(path, emoticon)
+   
+  result = load_library(path)
+  
+  result["get_emoticon"].each do |key, value|
+    if value == emoticon
+      result["get_emoticon"][value]
+    end 
+  end 
+  
 end
 
-def get_english_meaning
-  # code goes here
+# binding.pry
+
+def get_english_meaning(path, emoticon)
+  load_library(path)
 end
